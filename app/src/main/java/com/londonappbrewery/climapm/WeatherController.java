339ba01df7764ce25b7ba87405f91f11c0189f61
Tobibur <tobiburrahman786@gmail.com>
2018-06-27 15:56:52 +0530

    Project Updated

--- app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -33,7 +33,7 @@ public class WeatherController extends AppCompatActivity {
     final int REQUEST_CODE = 123;
     final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
     // App ID to use OpenWeather data
-    final String APP_ID = "b01809a2f59596cd62cb7257b86669ff";
+    final String APP_ID = "Your_API_KEY";
     // Time between location updates (5000 milliseconds or 5 seconds)
     final long MIN_TIME = 5000;
     // Distance between location updates (1000m or 1km)
